61f848bb53aa04b66dff4d8de4658331c8bdecd0
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2024-09-04 00:59:52 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ class Program {
 		Department dep = new Department(1, "Book");
 		
 		Seller seller = new Seller(1, "Lucas", "lucas@gmail", new Date(), 1000.00, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
