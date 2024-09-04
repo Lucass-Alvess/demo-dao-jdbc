7f6d5da7d0e0bdf41be7cd3478b59d27a9424217
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2024-09-04 19:44:38 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: selle update ===");
+		seller  = sellerDao.findbyId(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	
 
 	}
