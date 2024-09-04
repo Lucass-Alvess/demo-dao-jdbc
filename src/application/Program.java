103664d96355754886dc4c3882b1e67be97a18ed
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2024-09-04 01:38:32 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,13 @@ class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Book");
-		
-		Seller seller = new Seller(1, "Lucas", "lucas@gmail", new Date(), 1000.00, dep);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findbyId(3);
+		
 		System.out.println(seller);
+		
+	
 
 	}
 
